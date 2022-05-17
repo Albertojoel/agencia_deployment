593dd3393eaae6d0e385926ad6a274abb86ebc1b
Albertojoel <68121487+Albertojoel@users.noreply.github.com>
2022-05-16 21:18:37 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import router from "./routes/index.js";
+import db from './config/db.js';
+
+import dotenv from 'dotenv';
+dotenv.config({path:"variables.env"});
+
+const app = express();
+
+//Conectar la base de datos
+db.authenticate()
+    .then(() => console.log('Base de datos conectada'))
+    .catch(error => console.log(error));
+
+
+//Definir puerto
+const port = process.env.PORT || 4000
+const host = process.env.HOST || '0.0.0.0';
+
+//Habilitar pug
+app.set('view engine', 'pug');
+
+//Obtener el año actual
+app.use((req, res, next) =>{
+
+    const year = new Date();
+
+    res.locals.year = year.getFullYear();
+    res.locals.sitename = "Agencias de Viajes";
+
+
+    next();
+})
+//Agregar bodyparser para leer datos del formulario
+
+app.use(express.urlencoded({extended: true}));
+
+//Definir la carpeta publica
+app.use(express.static('public'));
+
+//Agregar routers
+app.use('/', router);
+
+
+app.listen(port, host, () =>{
+    console.log(`El servidor esta funcionando en el puerto ${port}`)
+})
